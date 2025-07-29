f942dfcd65792171d3f2c0c48244ac98642c4c69
Can Yıldız <abbas.yildiz@std.ankaramedipol.edu.tr>
2025-07-29 15:44:28 +0300

    Update User.java

--- backend/smartiot/smartiot/src/main/java/com/example/smartiot/model/User.java
+++ backend/smartiot/smartiot/src/main/java/com/example/smartiot/model/User.java
@@ -9,6 +9,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -21,4 +22,7 @@ public class User {
 
     @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "active")
+    private boolean active;  // true = aktif kullanıcı, false = pasif kullanıcı
 }
